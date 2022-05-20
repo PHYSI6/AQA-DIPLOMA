33a5b8192a4c7321ea5855b70f9a57621d9172fe
PHYSI6 <mr.vakban@mail.ru>
2022-05-21 00:15:54 +0300

    NO-CASE: Implemented Faker class for Project

---
+++ AQA DIPLOMA/AQA DIPLOMA/Fakers/ProjectFaker.cs
@@ -0,0 +1,13 @@
+﻿using AQA_DIPLOMA.Models;
+using Bogus;
+
+namespace AQA_DIPLOMA.Fakers;
+
+public sealed class ProjectFaker : Faker<Project>
+{
+    public ProjectFaker(int nameLenght)
+    {
+        RuleFor(b => b.Name, f => f.Random.String2(nameLenght));
+        RuleFor(b => b.Description, f => f.Company.CatchPhrase());
+    }
+}
