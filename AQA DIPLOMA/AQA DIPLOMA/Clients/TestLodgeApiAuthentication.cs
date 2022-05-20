e01bd0e6174e659e69bef0106fb166e8e2128643
PHYSI6 <mr.vakban@mail.ru>
2022-05-20 19:48:36 +0300

    NO-CASE: Implemented Clients

---
+++ AQA DIPLOMA/AQA DIPLOMA/Clients/TestLodgeApiAuthentication.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace AQA_DIPLOMA.Clients;
+
+public class TestLodgeApiAuthentication : IAuthenticator
+{
+    private string? Token { get; }
+
+    public TestLodgeApiAuthentication(string? token)
+    {
+        Token = token;
+    }
+
+    public ValueTask Authenticate(RestClient client, RestRequest request)
+    {
+        if (Token != null) request.AddHeader("Token", Token);
+        return ValueTask.CompletedTask;
+    }
+}
