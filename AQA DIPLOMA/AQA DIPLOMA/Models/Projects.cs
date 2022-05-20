b448c0b4c5a557f00e6a6621da1221d924c7ee3a
PHYSI6 <mr.vakban@mail.ru>
2022-05-20 20:34:28 +0300

    NO-CASE: Implemented Models

---
+++ AQA DIPLOMA/AQA DIPLOMA/Models/Projects.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AQA_DIPLOMA.Models;
+
+public record Projects
+{
+    [JsonPropertyName("projects")] public Project[]? ProjectsList { get; set; }
+}
