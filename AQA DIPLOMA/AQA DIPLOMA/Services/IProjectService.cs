923fef05d622c45e786571e0f2d10fd7f27cdf3f
PHYSI6 <mr.vakban@mail.ru>
2022-05-20 23:04:39 +0300

    NO-CASE: Implemented Project Services

---
+++ AQA DIPLOMA/AQA DIPLOMA/Services/IProjectService.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using AQA_DIPLOMA.Models;
+
+namespace AQA_DIPLOMA.Services;
+
+public interface IProjectService
+{
+    Task<Project> Show(int projectId);
+    Task<Projects> List();
+    Task<Project> Create(Project project);
+    HttpStatusCode Delete(int projectId);
+}
---
+++ AQA DIPLOMA/AQA DIPLOMA/Services/ProjectService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using AQA_DIPLOMA.Clients;
+using AQA_DIPLOMA.Models;
+using RestSharp;
+
+namespace AQA_DIPLOMA.Services;
+
+public class ProjectService : IProjectService, IDisposable
+{
+    private readonly RestClientExtended _client;
+    public ProjectService(RestClientExtended client)
+    {
+        _client = client;
+    }
+
+    public Task<Project> Show(int projectId)
+    {
+        var request = new RestRequest("/projects/{id}.json")
+            .AddUrlSegment("id", projectId);
+        
+        return _client.ExecuteAsync<Project>(request);
+    }
+
+    public Task<Projects> List()
+    {
+        var request = new RestRequest("/projects.json");
+
+        var projects = _client.ExecuteAsync<Projects>(request); 
+        return projects;
+    }
+
+    public Task<Project> Create(Project project)
+    {
+        var request = new RestRequest("/projects.json", Method.Post)
+            .AddJsonBody(project);
+        
+        return _client.ExecuteAsync<Project>(request);
+    }
+
+    public HttpStatusCode Delete(int projectId)
+    {
+        var request = new RestRequest("/projects/{id}.json", Method.Delete)
+            .AddUrlSegment("id", projectId)
+            .AddJsonBody("{}");
+        
+        return _client.ExecuteAsync(request).Result.StatusCode;
+    }
+
+    public void Dispose()
+    {
+        _client.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
