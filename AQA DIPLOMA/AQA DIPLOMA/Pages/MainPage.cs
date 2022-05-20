9cc42bbbec1e8e9ea9038c95f09380f78a7e1e85
PHYSI6 <mr.vakban@mail.ru>
2022-05-20 19:00:26 +0300

    NO-CASE: Implemented test pages

---
+++ AQA DIPLOMA/AQA DIPLOMA/Pages/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using AQA_DIPLOMA.Configuration;
+using OpenQA.Selenium;
+
+namespace AQA_DIPLOMA.Pages;
+
+public class MainPage : BasePage
+{
+    private const string PATH_SEGMENT = "/";
+    private static readonly By ButtonLoginBy = By.ClassName("login_link");
+    public IWebElement ButtonLogin => WaitService.WaitElementIsExists(ButtonLoginBy);
+
+    public MainPage(IWebDriver driver) : base(driver)
+    {
+    }
+
+    protected override By GetPageIdentifier()
+    {
+        return ButtonLoginBy;
+    }
+
+    public void OpenPage()
+    {
+        Driver.Navigate().GoToUrl(Configurator.AppSettings.BaseUrl + PATH_SEGMENT);
+    }
+}
---
+++ AQA DIPLOMA/AQA DIPLOMA/Pages/ProjectOverviewPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace AQA_DIPLOMA.Pages;
+
+public class ProjectOverviewPage : BasePage
+{
+    private static readonly By PageHeadingBy = By.TagName("h1");
+    private static readonly By ProjectCreationStatusBy = By.XPath("//div[@class='flash_notification']//p");
+    public IWebElement PageHeading => WaitService.WaitElementIsExists(PageHeadingBy);
+    public IWebElement ProjectCreationStatus => WaitService.WaitElementIsExists(ProjectCreationStatusBy);
+
+    public ProjectOverviewPage(IWebDriver driver) : base(driver)
+    {
+    }
+
+    protected override By GetPageIdentifier()
+    {
+        return PageHeadingBy;
+    }
+}
