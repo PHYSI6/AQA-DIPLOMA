b61b017db1211cc1422dda8c838f9288ea84c7a1
PHYSI6 <mr.vakban@mail.ru>
2022-05-23 23:19:21 +0300

    CASE-03: Implemented Login- and BaseStep classes

---
+++ AQA DIPLOMA/AQA DIPLOMA/Steps/LoginSteps.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using OpenQA.Selenium;
+
+namespace AQA_DIPLOMA.Steps;
+
+public class LoginSteps : BaseStep
+{
+    public LoginSteps(IWebDriver driver) : base(driver)
+    {
+    }
+
+    public void OpenMainPage()
+    {
+        MainPage.OpenPage();
+        MainPage.PageOpened.Should().BeTrue();
+    }
+
+    public void ClickButtonLoginOnMainPage()
+    {
+        MainPage.ButtonLogin.Click();
+        LoginNavigatorPage.PageOpened.Should().BeTrue();
+    }
+
+    public void ClickButtonLoginOnNavigatorPage()
+    {
+        LoginNavigatorPage.LoginButton.Click();
+        LoginPage.PageOpened.Should().BeTrue();
+    }
+
+    public void InputUsernameAndPassword(string username, string password)
+    {
+        LoginPage.UsernameField.SendKeys(username);
+        LoginPage.PasswordField.SendKeys(password);
+    }
+
+    public void ClickButtonContinue()
+    {
+        LoginPage.ContinueButton.Click();
+    }
+
+    public void AuthorizationSuccessCheck()
+    {
+        ProjectsPage.PageOpened.Should().BeTrue();
+    }
+
+    public void AuthorizationErrorCheck()
+    {
+        LoginPage.LoginErrorMessage.Displayed.Should().BeTrue();
+    }
+}
