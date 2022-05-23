fd505d27e397a3691beca5b1af80098818af61f7
PHYSI6 <mr.vakban@mail.ru>
2022-05-23 23:20:40 +0300

    CASE-03: Implemented BaseTest for UI testing

---
+++ AQA DIPLOMA/AQA DIPLOMA/Tests/Ui/BaseTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AQA_DIPLOMA.Services;
+using AQA_DIPLOMA.Steps;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace AQA_DIPLOMA.Tests.Ui;
+
+public class BaseTest
+{
+    [ThreadStatic] private static IWebDriver _driver;
+    protected LoginSteps LoginSteps;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _driver = new BrowserService().Driver;
+        LoginSteps = new LoginSteps(_driver);
+    }
+    
+    [TearDown]
+    public void TearDown()
+    {
+        _driver.Quit();
+    }
+    
+    protected static IWebDriver Driver
+    {
+        get => _driver;
+        set => _driver = value ?? throw new ArgumentNullException(nameof(value));
+    }
+}
