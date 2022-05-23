e7715f9e5ded1cc35ab90b0e99749127fbbb62d4
PHYSI6 <mr.vakban@mail.ru>
2022-05-23 23:22:38 +0300

    CASE-03: Added "Login error message" to LoginPage

--- AQA DIPLOMA/AQA DIPLOMA/Pages/LoginPage.cs
+++ AQA DIPLOMA/AQA DIPLOMA/Pages/LoginPage.cs
@@ -7,9 +7,11 @@ public class LoginPage : BasePage
     private static readonly By UsernameFieldBy = By.Id("username");
     private static readonly By PasswordFieldBy = By.Id("password");
     private static readonly By ContinueButtonBy = By.Name("action");
+    private static readonly By LoginErrorMessageBy = By.Id("error-element-password");
     public IWebElement UsernameField => WaitService.WaitElementIsExists(UsernameFieldBy);
     public IWebElement PasswordField => WaitService.WaitElementIsExists(PasswordFieldBy);
     public IWebElement ContinueButton => WaitService.WaitElementIsExists(ContinueButtonBy);
+    public IWebElement LoginErrorMessage => WaitService.WaitElementIsExists(LoginErrorMessageBy);
 
     public LoginPage(IWebDriver driver) : base(driver)
     {
