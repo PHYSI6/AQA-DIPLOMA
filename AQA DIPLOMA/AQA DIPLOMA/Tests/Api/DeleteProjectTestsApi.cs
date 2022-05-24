38b52ce3d50a77e3463fe06a4bb0b1cb5886158e
PHYSI6 <mr.vakban@mail.ru>
2022-05-24 20:07:27 +0300

    CASE-05: Implemented test for project deletion API

---
+++ AQA DIPLOMA/AQA DIPLOMA/Tests/Api/DeleteProjectTestsApi.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using AQA_DIPLOMA.Fakers;
+using AQA_DIPLOMA.Models;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AQA_DIPLOMA.Tests.Api;
+
+public class DeleteProjectTestsApi : BaseTestApi
+{
+    private Project? _project;
+    
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _project = new ProjectFaker();
+        var actualProject = ProjectService?.Create(_project).Result;
+        _project = actualProject;
+    }
+    
+    [Test]
+    public void Delete_Existing_Project()
+    {
+        var deleteStatus = ProjectService.Delete(_project.Id);
+        deleteStatus.Should().Be(HttpStatusCode.NoContent);
+    }
+
+    [Test]
+    public void Delete_Non_Existing_Project()
+    {
+        var nonExisingProjectId = -2;
+        var deleteStatus = ProjectService.Delete(nonExisingProjectId);
+        deleteStatus.Should().Be(HttpStatusCode.NotFound);
+    }
+}
