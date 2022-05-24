acdf703aa6f290e6d212fb8d58c74bc780e81a6f
PHYSI6 <mr.vakban@mail.ru>
2022-05-24 14:29:39 +0300

    CASE-06: Implemented new project addition tests

---
+++ AQA DIPLOMA/AQA DIPLOMA/Tests/Ui/AddNewProjectTests.cs
@@ -0,0 +1,45 @@
+﻿using AQA_DIPLOMA.Configuration;
+using AQA_DIPLOMA.Fakers;
+using AQA_DIPLOMA.Models;
+using NUnit.Framework;
+
+namespace AQA_DIPLOMA.Tests.Ui;
+
+public class AddNewProjectTests: BaseTest
+{
+    private Project? _project;
+    [Test]
+    [TestCase(1) ,TestCase(149), TestCase(150)]
+    public void Add_new_project(int lenghtOfProjectName)
+    {
+        LoginSteps.OpenMainPage();
+        LoginSteps.ClickButtonLoginOnMainPage();
+        LoginSteps.ClickButtonLoginOnNavigatorPage();
+        LoginSteps.InputUsernameAndPassword(Configurator.Admin.Email, Configurator.Admin.Password);
+        LoginSteps.ClickButtonContinue();
+        LoginSteps.AuthorizationSuccessCheck();
+        AddProjectSteps.ClickButtonNewProjectOnProjectPage();
+        _project  = new ProjectFaker(lenghtOfProjectName);
+        AddProjectSteps.InputNewProjectName(_project.Name);
+        AddProjectSteps.ClickButtonAddProject();
+        AddProjectSteps.ProjectAdditionSuccessCheck();
+    }
+    
+    [Test]
+    [TestCase(151, "Name is too long (maximum is 150 characters)")]
+    [TestCase(0, "Name can't be blank")]
+    public void Add_new_proj(int lenghtOfProjectName, string errorText)
+    {
+        LoginSteps.OpenMainPage();
+        LoginSteps.ClickButtonLoginOnMainPage();
+        LoginSteps.ClickButtonLoginOnNavigatorPage();
+        LoginSteps.InputUsernameAndPassword(Configurator.Admin.Email, Configurator.Admin.Password);
+        LoginSteps.ClickButtonContinue();
+        LoginSteps.AuthorizationSuccessCheck();
+        AddProjectSteps.ClickButtonNewProjectOnProjectPage();
+        _project  = new ProjectFaker(lenghtOfProjectName);
+        AddProjectSteps.InputNewProjectName(_project.Name);
+        AddProjectSteps.ClickButtonAddProject();
+        AddProjectSteps.ProjectAdditionErrorCheck(errorText);
+    }
+}
