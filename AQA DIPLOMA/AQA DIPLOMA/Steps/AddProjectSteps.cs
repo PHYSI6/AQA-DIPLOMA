68b6ae7037bbfe94ee238b73bc1911a67d5decf0
PHYSI6 <mr.vakban@mail.ru>
2022-05-24 13:59:15 +0300

    CASE-06: Implemented steps for project addition

---
+++ AQA DIPLOMA/AQA DIPLOMA/Steps/AddProjectSteps.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using OpenQA.Selenium;
+
+namespace AQA_DIPLOMA.Steps;
+
+public class AddProjectSteps: BaseStep
+{
+    public AddProjectSteps(IWebDriver driver) : base(driver)
+    {
+    }
+
+    public void ClickButtonNewProjectOnProjectPage()
+    {
+        ProjectsPage.NewProjectButton.Click();
+        NewProjectPage.PageOpened.Should().BeTrue();
+    }
+
+    public void InputNewProjectName(string? name)
+    {
+        NewProjectPage.ProjectName.SendKeys(name);
+    }
+
+    public void ClickButtonAddProject()
+    {
+        NewProjectPage.ButtonAddProject.Click();
+    }
+    
+    public void ProjectAdditionSuccessCheck()
+    {
+        using (new AssertionScope())
+        {
+            ProjectOverviewPage.PageOpened.Should().BeTrue();
+            ProjectOverviewPage.ProjectCreationStatus.Text.Should().Be("Project was successfully created.");
+        }
+    }
+
+    public void ProjectAdditionErrorCheck(string errorMessage)
+    {
+        using (new AssertionScope())
+        {
+            NewProjectPage.PageOpened.Should().BeTrue();
+            NewProjectPage.ErrorMessage.Displayed.Should().BeTrue();
+            NewProjectPage.ErrorMessageText.Text.Should().Be(errorMessage);
+        }
+    }
+}
